32548ff9de4b93b98fdc0f9d3a338ecee4747c6d
aleksandreleskin <eleskin1201@gmail.com>
2021-02-17 12:42:49 +0300

    0.1.0

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,37 +6,68 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/wallet',
     name: 'Wallet',
     component: () => import(/* webpackChunkName: "about" */ '../views/Wallet.vue'),
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/history',
     name: 'History',
     component: () => import(/* webpackChunkName: "about" */ '../views/History.vue'),
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/settings',
     name: 'Settings',
     component: () => import(/* webpackChunkName: "about" */ '../views/Settings.vue'),
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/login',
     name: 'Login',
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
     path: '/register',
     name: 'Register',
     component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue'),
+    meta: {
+      requiresAuth: false,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    const isAuth = true;
+    if (isAuth) {
+      next({ name: 'Login' });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
